b2c559929de512f46ecf7e74f33157c9009da124
WBFernandez <66105242+WBFernandez@users.noreply.github.com>
2024-11-20 08:55:22 +0800

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Retrieve the password from secrets
+PASSWORD = st.secrets["general"]["password"]
+
+# Password prompt
+password = st.text_input("Enter the password:", type="password")
+
+if password == PASSWORD:
+    st.write("Welcome to the app!")
+    # Add your app content here
+else:
+    st.warning("Invalid password, please try again.")
